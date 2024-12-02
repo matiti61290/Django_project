cbd885ae6e0d0dc3a600d788147b38ccc2a8bc0f
Mathieu <mathieu.barbey18@gmail.com>
2024-12-02 11:11:46 +0100

    correction des noms de champs

--- bibliothecaire/forms.py
+++ bibliothecaire/forms.py
@@ -2,23 +2,23 @@ from django import forms
 from bibliothecaire.models import Emprunteur
 
 class CreationLivre(forms.Form):
-    nameLivre = forms.CharField(required=True)
-    auteur = forms.CharField(required=True)
+    nameLivre = forms.CharField(required=True, label='Nom livre')
+    auteur = forms.CharField(required=True, label='Auteur')
 
 class CreationDvd(forms.Form):
-    nameDVD = forms.CharField(required=True)
-    realisateur = forms.CharField(required=True)
+    nameDVD = forms.CharField(required=True, label='Nom DVD')
+    realisateur = forms.CharField(required=True, label='Réalisateur')
 
 class CreationCd(forms.Form):
-    nameCD = forms.CharField(required=True)
-    artiste = forms.CharField(required=True)
+    nameCD = forms.CharField(required=True, label='Nom CD')
+    artiste = forms.CharField(required=True, label='Artiste')
 
 class CreationJeuDePlateau(forms.Form):
-    name = forms.CharField(required=True)
-    createur = forms.CharField(required=True)
+    name = forms.CharField(required=True, label='Nom du jeu')
+    createur = forms.CharField(required=True, label='Créateur')
 
 class CreationMembre(forms.Form):
-    name = forms.CharField(required=True)
+    name = forms.CharField(required=True, label='Nom du membre')
 
 class EmpruntLivre(forms.Form):
     disponible = forms.BooleanField(required=True)
